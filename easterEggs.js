0624fc7818d7434cb4abc62bf70f5acede443fc9
Mark Coleman <markcoleman@gmail.com>
2025-01-05 09:39:11 -0500

    added in story extract logic (#13)

--- easterEggs.js
+++ easterEggs.js
@@ -1,12 +1,22 @@
 // easterEggs.js
-import chalk from 'chalk';
-import { logWithDelay } from './logWithDelay.js';
+import chalk from "chalk";
+import { logWithDelay } from "./logWithDelay.js";
 
 // Easter Egg #1: RUN_EASTER_EGG
 export async function runEasterEgg() {
   if (process.env.RUN_EASTER_EGG) {
-    await logWithDelay(chalk.bgMagenta("🔎 Easter Egg #1 found: RUN_EASTER_EGG variable is set!"), 0);
-    await logWithDelay(chalk.magenta("The code fairies grant you extra GitHub contributions today!\n"), 0);
+    await logWithDelay(
+      chalk.bgMagenta(
+        "🔎 Easter Egg #1 found: RUN_EASTER_EGG variable is set!",
+      ),
+      0,
+    );
+    await logWithDelay(
+      chalk.magenta(
+        "The code fairies grant you extra GitHub contributions today!\n",
+      ),
+      0,
+    );
   }
 }
 
@@ -21,14 +31,38 @@ export async function maybeShowSanta(santaClausArt) {
 // Easter Egg #3: ONCALL
 export async function onCallEasterEgg() {
   if (process.env.ONCALL === "true") {
-    await logWithDelay(chalk.red("🔔 Easter Egg #3: You are on-call tonight!"), 0);
-    await logWithDelay(chalk.red("Better keep one eye on your pager, just in case of an alert.\n"), 0);
+    await logWithDelay(
+      chalk.red("🔔 Easter Egg #3: You are on-call tonight!"),
+      0,
+    );
+    await logWithDelay(
+      chalk.red(
+        "Better keep one eye on your pager, just in case of an alert.\n",
+      ),
+      0,
+    );
   }
 }
 
 // Easter Egg #4: Rare debug log
 export async function debugEasterEgg() {
   if (Math.random() < 0.1) {
-    await logWithDelay(chalk.bgBlack.white("\n*** Debug Log: Santa has spontaneously triggered a hotfix ***\n"), 0);
+    await logWithDelay(
+      chalk.bgBlack.white(
+        "\n*** Debug Log: Santa has spontaneously triggered a hotfix ***\n",
+      ),
+      0,
+    );
   }
+}
+
+export async function codeCoverage() {
+  // Easter Egg #5: CODE_COVERAGE
+  const coverage = process.env.CODE_COVERAGE || "92.3%";
+  await logWithDelay(
+    chalk.greenBright(
+      `PS: Santa says code coverage is at ${coverage}... so you’ve been *mostly* good this year!\n`,
+    ),
+    0,
+  );
 }
